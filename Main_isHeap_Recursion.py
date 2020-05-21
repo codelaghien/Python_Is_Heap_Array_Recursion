252185e1b11ba21a466b9724f81399c31ef2953e
codelaghien <codelaghien.github.io>
2020-05-20 20:48:17 -0700

    kiem tra xem 1 mang la Binary Min-Heap dung Recursion - de quy

---
+++ Main_isHeap_Recursion.py
@@ -0,0 +1,22 @@
+print("Chương trình kiểm tra Binary Min-Heap dùng đệ quy!")
+
+
+def is_heap(arr, index, length):
+    left = index * 2 + 1
+    right = left + 1
+    if left < length and arr[index] > arr[left]:
+        return False
+    if right < length and array[index] > array[right]:
+        return False
+
+    left_ok = left > length - 1 or is_heap(arr, left, length)
+    right_ok = right > length - 1 or is_heap(arr, right, length)
+    return left_ok and right_ok
+
+
+array = [1, 5, 3, 7, 9, 8, 4]
+
+if is_heap(array, 0, len(array)):
+    print('array is a binary min-heap')
+else:
+    print('array is NOT a binary min-heap')
